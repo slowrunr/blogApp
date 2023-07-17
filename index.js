9bbce55de21bf235056c87794b5b8b50f5c7cf0f
slowrunr <mungaslov.nft@gmail.com>
2023-07-18 00:21:36 +0900

    added more functions in index.js

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
-let postTitle = null;
+let post = null;
 
 const titleInputNode = document.getElementById("titleInput");
 const postBtnNode = document.getElementById("postBtn");
 const postedTitleNode = document.getElementById("postedTitle");
 
 postBtnNode.addEventListener("click", function () {
+  // 1 вариант кода
   //console.log("looks good");
-  postTitle = titleInputNode.value;
+  //postTitle = titleInputNode.value;
+  //  console.log(postTitle);
+  //postedTitleNode.innerText = postTitle;
 
-  console.log(postTitle);
+  // 2-й вариант кода с разбивкой по задачам (рефкторинг)
 
-  postedTitleNode.innerText = postTitle;
+  // сначала получаем данные из поля вводда
+  const postFromUser = getPostFromUser();
+
+  // затем сохраняем пост функцией setPost
+  savePost(postFromUser);
+
+  // и отображаем текст в postedTitle
+  renderPost();
 });
 
-title = "first title";
-console.log(title);
+function getPostFromUser() {
+  const post = titleInputNode.value;
+  return post;
+}
+
+function savePost(newPost) {
+  post = newPost;
+}
+
+function renderPost() {
+  postedTitleNode.innerText = post;
+}
