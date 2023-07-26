1bf39017b22b0542e974d0f69bcd592d17f3c8da
slowrunr <mungaslov.nft@gmail.com>
2023-07-27 00:29:30 +0900

    trying to add 'time_ago' function. undone

--- index.js
+++ index.js
@@ -72,12 +72,44 @@ function getPostFromUser() {
   };
 }
 
+// пробуем добавить функционал с записью "кол-во минут назад". Взято из https://stackoverflow.com/questions/3177836/how-to-format-time-since-xxx-e-g-4-minutes-ago-similar-to-stack-exchange-site
+function formatedDate(date) {
+  const dt = new Date();
+  let dayOfMonth = dt.getDate();
+  let month = dt.getMonth() + 1;
+  let year = dt.getFullYear();
+  let hour = dt.getHours();
+  let minutes = dt.getMinutes();
+  let diffMs = new Date() - dt;
+  let diffSec = Math.round(diffMs / 1000);
+  let diffMin = diffSec / 60;
+  let diffHour = diffMin / 60;
+
+  // форматирование
+  year = year.toString().slice(-2);
+  month = month < 10 ? "0" + month : month;
+  dayOfMonth = dayOfMonth < 10 ? "0" + dayOfMonth : dayOfMonth;
+  hour = hour < 10 ? "0" + hour : hour;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+
+  if (diffSec < 1) {
+    return "только что";
+  } else if (diffMin < 1) {
+    return `${diffSec} секунд назад`;
+  } else if (diffHour < 1) {
+    return `${diffMin} минут назад`;
+  } else {
+    return `${dayOfMonth}.${month}.${year} ${hour}:${minutes}`;
+  }
+}
+
 //function savePost(newPost) { -  эта функция была нужна, чтобы сохранять новый пост - меняем её на addpost
 //  post = newPost;}
 
 function addPost({ title, text }) {
+  const currentDate = formatedDate();
   // === addPost(post)
-  const currentDate = new Date();
+  //const currentDate = new Date();
   // читается как "присвоить экземпляр объекта Date"
 
   posts.push({
@@ -99,10 +131,7 @@ function renderPosts() {
   posts.forEach((post) => {
     postsHTML += `
       <div class="post">
-        <p class="post__date">${post.currentDate.toLocaleDateString()} ${post.currentDate.toLocaleTimeString(
-      [],
-      { hour: "2-digit", minute: "2-digit" }
-    )}</p>
+        <p class="post__date">${post.currentDate}</p>
         <p class="post__title">${post.title}</p>
         <p class="post__text">${post.text}</p>
       </div>
@@ -113,4 +142,4 @@ function renderPosts() {
 
   //console.log(getPost());
   //postedTitleNode.innerText = post;
-}
+} //${post.currentDate.toLocaleDateString()} ${post.currentDate.toLocaleTimeString(  [],  { hour: "2-digit", minute: "2-digit" })}
